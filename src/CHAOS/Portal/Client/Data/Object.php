00e6bff48b2fc5b97322ab7f1ed019f66f185c9d
Kræn Hansen <post@creen.dk>
2013-07-18 14:38:28 +0200

    Added comments and a getter method for the raw object.

--- src/CHAOS/Portal/Client/Data/Object.php
+++ src/CHAOS/Portal/Client/Data/Object.php
@@ -12,22 +12,52 @@ namespace CHAOS\Portal\Client\Data;
  */
 class Object
 {
+	/**
+	 * The object returned from CHAOS.
+	 * @var stdClass
+	 */
 	protected $_object;
+	
+	/**
+	 * Create a wrapper that wraps an object from CHAOS.
+	 * @param \stdClass $object
+	 */
 	public function __construct(\stdClass $object) {
 		$this->_object = $object;
+	}
+	
+	/**
+	 * Returns the object given when constructed.
+	 * @return stdClass
+	 */
+	public function getObject() {
+		return $this->_object;
 	}
 	
 	/**
 	 * An array holding the $prefix -> $ns.
 	 * @var string[string]
 	 */
-	
 	protected static $xml_namespaces = array();
 	
+	/**
+	 * Register a namespace to use when initializing SimpleXMLDocuments.
+	 * @param string $prefix The prefix that the namespace should be registered under.
+	 * @param string $ns The URI of the namespace to register.
+	 */
 	public static function registerXMLNamespace($prefix, $ns) {
 		self::$xml_namespaces[$prefix] = $ns;
 	}
 	
+	/**
+	 * Returns the first match of occurance of $xpath in a metadata blob complient with the schema of GUID = $schema_guid.
+	 * @param string|string[] $schema_guids GUID(s) for schemas to search in.
+	 * @param string|string[] $xpaths XPath expression(s) to search for.
+	 * @param string $seperator If multiple matches are found, what seperator should concatinate the results? (default = ', ')
+	 * 							If $seperator == null, an array is returned instead.
+	 * @throws \InvalidArgumentException If $schema_guids or $xpaths are not both arrays of equal size or strings.
+	 * @return string|stdClass[]|NULL A string concatinated with $seperator, an array of matches or NULL if nothing was found.
+	 */
 	public function metadata($schema_guids, $xpaths, $seperator = ', ') {
 		if(is_string($schema_guids)) {
 			$schema_guids = array($schema_guids);
@@ -47,7 +77,7 @@ class Object
 		
 		for($i = 0; $i < count($schema_guids); $i++) {
 			$value = $this->get_metadata($schema_guids[$i], $xpaths[$i]);
-			if(is_string($value)) {
+			if(isset($value)) {
 				return $value;
 			}
 		}
@@ -60,6 +90,15 @@ class Object
 	 */
 	protected $xml_cache = array();
 	
+	/**
+	 * Returns occurances of $xpath in a metadata blob complient with the schema of GUID = $schema_guid.
+	 * @param string $schema_guid GUID for schema to search in.
+	 * @param string $xpath XPath expression to search for.
+	 * @param string $seperator If multiple matches are found, what seperator should concatinate the results? (default = ', ')
+	 * 							If $seperator == null, an array is returned instead.
+	 * @throws \RuntimeException If a namespace couldn't be registered.
+	 * @return NULL|string|stdClass[] A string concatinated with $seperator, an array of matches or NULL if nothing was found.
+	 */
 	protected function get_metadata($schema_guid, $xpath, $seperator = ', ') {
 		$schema_guid = strtolower($schema_guid);
 		foreach($this->_object->Metadatas as $metadata) {
@@ -73,7 +112,13 @@ class Object
 					}
 				}
 				$node = $this->xml_cache[$schema_guid]->xpath($xpath);
-				return implode($seperator, $node);
+				if(count($node) == 0) {
+					return null;
+				} elseif(isset($seperator)) {
+					return implode($seperator, $node);
+				} else {
+					return $node;
+				}
 			}
 		}
 	}
