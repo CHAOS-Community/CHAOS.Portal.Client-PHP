c310dbb6d91699ee4cce6df974b8ffb0d18a5952
Markus Færevaag <mafaer@gmail.com>
2013-10-15 09:49:50 +0200

    Added debugging to assertSuccess

--- tests/PortalClientTest.php
+++ tests/PortalClientTest.php
@@ -32,13 +32,33 @@ class PortalClientTest extends PHPUnit_Framework_TestCase
     /**
      * Asserts successfull ServiceResult
      *
-     * @param  ServiceResult $serviceResult
+     * @param  \CHAOS\Portal\Client\Data\ServiceResult $serviceResult
      * @throws PHPUnit_Framework_AssertionFailedError
      */
 	public static function assertSuccess($serviceResult, $message = 'Successfully recieved ServiceResult')
 	{
-		self::assertTrue($serviceResult->WasSuccess(), $message);
-		self::assertTrue($serviceResult->MCM()->WasSuccess(), $message);
+		try
+		{
+			self::assertTrue($serviceResult->WasSuccess(), $message);
+			self::assertTrue($serviceResult->MCM()->WasSuccess(), $message);
+
+		}
+		catch (Exception $e)
+		{
+			$e_msg = $e->getMessage() . "\n\n";
+
+			if (!$serviceResult->WasSuccess()) {
+				$e_msg .= "Portal error: " . $serviceResult->Error()->Message() . "\n";
+			} else if (!$serviceResult->MCM()->WasSuccess()) {
+				$error = $serviceResult->MCM()->Error();
+
+				$e_msg .= "Exception: " . $error->Name() . "\n";
+				$e_msg .= "Message: " . $error->Message() . "\n";
+				$e_msg .= "Stacktrace: " . $error->Stacktrace() . "\n";
+			}
+
+			throw new PHPUnit_Framework_AssertionFailedError($e_msg);
+		}
 	}
 }
 ?>
