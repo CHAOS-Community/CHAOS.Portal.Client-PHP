5c245a047364ef9321e01e018b8b58ab4d04e4b0
Markus Færevaag <mafaer@gmail.com>
2013-10-17 10:43:11 +0200

    Added 'authenticated' field to fix funky GetByFolderID failure

--- tests/PortalClientTestCase.php
+++ tests/PortalClientTestCase.php
@@ -11,6 +11,7 @@ class PortalClientTestCase extends PHPUnit_Framework_TestCase
 	protected static $servicePath;
 	protected static $clientGUID;
 	protected static $accessPointGUID;
+	protected static $authenticated;
 
 	protected static $client;
 
@@ -25,13 +26,37 @@ class PortalClientTestCase extends PHPUnit_Framework_TestCase
 		self::$servicePath = self::$config['service_path'];
 		self::$clientGUID = self::$config['client_guid'];
 		self::$accessPointGUID = self::$config['access_point_guid'];
+		self::$authenticated = false;
 
 		self::$client = new PortalClient(self::$servicePath, self::$clientGUID);
 
 		if (isset(self::$config['email']) && isset(self::$config['password']))
 		{
-			self::$client->EmailPassword()->Login(self::$config['email'],
-			                                      self::$config['password']);
+			$serviceResult = self::$client->EmailPassword()->Login(
+				self::$config['email'],
+				self::$config['password']
+			);
+
+			try
+			{
+				self::assertEmpty($serviceResult->EmailPassword()->Error(), "Logged in without errors");
+
+			} catch (Exception $e)
+			{
+				$e_msg = "Login failed!\n\n";
+
+				$error = $serviceResult->EmailPassword()->Error();
+
+				$e_msg .= "Exception: " . $error->Name() . "\n";
+				$e_msg .= "Message: " . $error->Message() . "\n";
+				$e_msg .= "Stacktrace: \n" . $error->Stacktrace() . "\n";
+
+				echo $e_msg;
+				exit(1); // All tests are gonne fail anyways, so let's save some time and put it out of its missery.
+
+			} finally {
+				self::$authenticated = true;
+			}
 		}
 	}
 
@@ -58,23 +83,34 @@ class PortalClientTestCase extends PHPUnit_Framework_TestCase
 			self::assertTrue($serviceResult->WasSuccess(), $message);
 			self::assertTrue($serviceResult->MCM()->WasSuccess(), $message);
 
-		}
-		catch (Exception $e)
-		{
-			$e_msg = $e->getMessage() . "\n\n";
+		} catch (Exception $e)
+			{
+				$e_msg = $e->getMessage() . "\n\n";
 
-			if (!$serviceResult->WasSuccess()) {
-				$e_msg .= "Portal error: " . $serviceResult->Error()->Message() . "\n";
-			} else if (!$serviceResult->MCM()->WasSuccess()) {
-				$error = $serviceResult->MCM()->Error();
+				if (!$serviceResult->WasSuccess()) {
+					$e_msg .= "Portal error: " . $serviceResult->Error()->Message() . "\n";
+				} else if (!$serviceResult->MCM()->WasSuccess()) {
+					$error = $serviceResult->MCM()->Error();
 
-				$e_msg .= "Exception: " . $error->Name() . "\n";
-				$e_msg .= "Message: " . $error->Message() . "\n";
-				$e_msg .= "Stacktrace: \n" . $error->Stacktrace() . "\n";
+					$e_msg .= "Exception: " . $error->Name() . "\n";
+					$e_msg .= "Message: " . $error->Message() . "\n";
+					$e_msg .= "Stacktrace: \n" . $error->Stacktrace() . "\n";
+				}
+
+				throw new PHPUnit_Framework_AssertionFailedError($e_msg);
 			}
+	}
 
-			throw new PHPUnit_Framework_AssertionFailedError($e_msg);
-		}
+    /**
+     * Asserts unsuccessfull ServiceResult
+     *
+     * @param  \CHAOS\Portal\Client\Data\ServiceResult $serviceResult
+     * @throws PHPUnit_Framework_AssertionFailedError
+     */
+	public static function assertNotSuccess($serviceResult, $message = 'Successfully recieved ServiceResult')
+	{
+		self::assertFalse($serviceResult->WasSuccess(), $message);
+		self::assertFalse($serviceResult->MCM()->WasSuccess(), $message);
 	}
 }
 ?>
