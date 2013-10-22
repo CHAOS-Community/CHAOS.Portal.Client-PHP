3dd11934907b3ed7862d5673dd5bdfa001439287
Markus Færevaag <mafaer@gmail.com>
2013-10-22 21:16:18 +0200

    Fixed and tested FolderTypeExtension#Get

--- src/CHAOS/Portal/Client/Extensions/FolderTypeExtension.php
+++ src/CHAOS/Portal/Client/Extensions/FolderTypeExtension.php
@@ -1,13 +1,12 @@
 <?php
 	namespace CHAOS\Portal\Client\Extensions;
 	use \CHAOS\Portal\Client\IServiceCaller;
-	
+
 	class FolderTypeExtension extends AExtension implements IFolderTypeExtension
 	{
 		public function Get($id = null, $name = null)
 		{
-			return $this->CallService("Set", IServiceCaller::GET, array("id" => $id,
-																		"name" => $name));
+			return $this->CallService("Get", IServiceCaller::GET, array("id" => $id, "name" => $name));
 		}
 	}
 ?>
--- tests/FolderTypeExtensionTest.php
+++ tests/FolderTypeExtensionTest.php
@@ -5,12 +5,18 @@ class FolderTypeExtensionTest extends PortalClientTestCase
 {
 	public function testGet()
 	{
+		$expected = self::$data['folder_type'][0];
+
 		$serviceResult = self::$client->FolderType()->Get(
-			self::$data['folder'][0]['folder_type_id']
+			$expected['id']
 		);
 
 		$this->assertSuccess($serviceResult);
-		$this->assertSuccess($serviceResult->Portal());
+		$this->assertNotEmpty($serviceResult->MCM()->Results(), "Returned not empty results");
+
+		$actual = $serviceResult->MCM()->Results()[0];
+		$this->assertEquals($expected['name'], $actual->Name, 'Result has correct name');
+		$this->assertEquals($expected['date_created'], $actual->DateCreated, 'Result has correct date_created');
 	}
 }
 ?>
